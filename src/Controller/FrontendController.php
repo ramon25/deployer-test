81949c613aee6bdf2e7a84ef4779a79cf8257358
Ramon Rainer <ramon.rainer@ongoing.ch>
2020-02-11 10:09:21 +0100

    feat: add frontend controller

---
+++ src/Controller/FrontendController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontendController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/FrontendController.php',
+        ]);
+    }
+}
